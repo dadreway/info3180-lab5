1248adadd9e8606dae793343433d9ab85f8f5b6f
Andrew Hylton <andrew.hylton02@mymona.uwi.edu>
2017-02-26 17:33:44 +0000

    secoond commit

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
+    password = db.Column(db.String(255))
     username = db.Column(db.String(80), unique=True)
 
     def is_authenticated(self):
