c4187809c42e2c94cb885b71eb91e7104aace098
Andrew Hylton <andrew.hylton02@mymona.uwi.edu>
2017-02-26 18:01:23 +0000

    finsihed step Update the /login route

--- app/views.py
+++ app/views.py
@@ -28,25 +28,42 @@ def about():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        #if user is already logged in, redirects them to the secure page to prevent them
+        #From loogging in again.
+        return redirect(url_for('secure_page'))
+        
+        
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.username.data and form.password.data:
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
+            
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user = UserProfile.query.filter_by(username=username, password=password).first()
             # get user id, load into session
-            login_user(user)
+            if user is not None:
+                login_user(user)
+                flash('logged in successfully.','success')
+                next = request.args.get('next')
+                return redirect(url_for('secure_page'))
+            else:
+                flash('Username or Password is incorrect.','danger')
 
-            # remember to flash a message to the user
-            return redirect(url_for("home")) # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
+@app.route("/secure-page")
+@login_required
+def secure_page():
+    return render_template('securepage.html')
+    
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
